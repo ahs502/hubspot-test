27024c8d54b94a441cda5742331e02c326580ad0
Hessamoddin A Shokravi <ahs502@gmail.com>
2019-09-24 19:50:41 +0330

    Implement new feature page styles

--- src/pages/new-feature.jsx
+++ src/pages/new-feature.jsx
@@ -1,12 +1,59 @@
 import React, { Fragment } from "react";
 import Header from "../components/Header";
-import Body from "../components/Body";
+import { Typography, makeStyles, Grid, Paper } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  grid: {
+    height: "calc(100vh - 64px)"
+  },
+  leftPane: {
+    backgroundColor: "#ccc"
+  },
+  leftContainer: {
+    width: "60%",
+    marginLeft: "20%",
+    marginTop: 100
+  },
+  rightPane: {
+    // Nothing yet!
+  },
+  rightContainer: {
+    width: "40%",
+    marginLeft: "30%",
+    marginTop: 100
+  },
+  feature: {
+    marginBottom: theme.spacing(2),
+    padding: theme.spacing(2),
+    cursor: "pointer"
+  }
+}));
 
 const NewFeaturePage = () => {
+  const classes = useStyles();
+
   return (
     <Fragment>
-      <Header>New Feature</Header>
-      <Body>New Feature</Body>
+      <Header>
+        <Typography variant="h6">New Features</Typography>
+      </Header>
+      <Grid container spacing={0} className={classes.grid}>
+        <Grid item xs className={classes.leftPane}>
+          <div className={classes.leftContainer}>
+            <Typography variant="h4">New Features</Typography>
+            <Typography variant="body2">
+              sdjkal fsdlkgjas dfg lkasfdjg sflkgjksafjg sdakljg safjlk
+              ghsafjklgh kjsahg kjdsgkjlasfh; guf huehgajlrgh sfdj ghjksfdgakjh
+            </Typography>
+          </div>
+        </Grid>
+        <Grid item xs className={classes.rightPane}>
+          <div className={classes.rightContainer}>
+            <Paper className={classes.feature}>Something</Paper>
+            <Paper className={classes.feature}>Another thing</Paper>
+          </div>
+        </Grid>
+      </Grid>
     </Fragment>
   );
 };
